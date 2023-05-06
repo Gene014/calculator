dc32faa21db77a558d96e96c252c3752abfedfde
Gene014 <calmaeugene06@gmail.com>
2023-05-06 23:31:06 +0800

    concatenate problem solved addition operation now working

--- runcode.py
+++ runcode.py
@@ -6,13 +6,13 @@ while True:
     username=input("Hi, Good day! Please enter your name.")
 # Ask the user what kind of mathematical operation it would used. 
     print("Welcome to your Virtual Calculator! Enter the numbers and I'll solve it for you!")
-    first_number = input("Please enter your first number.").upper().replace(" ", "")
-    second_number = input("Please enter your second number.").upper().replace(" ", "")
+    first_number = int(input("Please enter your first number."))
+    second_number = int(input("Please enter your second number."))
     chosen_operation = input("Press 1 if you want to ADD the variables. Press 2 to SUBTRACT. Press 3 to MULTIPLY. Or press 4 to DIVIDE").upper().replace(" ", "")
-    if chosen_operation == 1:
-        print ("The sum is" + {first_number} + {second_number})
+    if chosen_operation == "1":
+        print (f"The sum is {first_number + second_number}")
         #input code for addition
-    elif chosen_operation == 2:
+    elif chosen_operation == "2":
         print("")
         #input code for addition
     elif chosen_operation == 3:
