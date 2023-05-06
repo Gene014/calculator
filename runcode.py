2174cb3cf9b42eee8ea3e5f8c627822591a314a2
Gene014 <calmaeugene06@gmail.com>
2023-05-06 17:15:55 +0800

    ask for the two numbers

--- runcode.py
+++ runcode.py
@@ -1,5 +1,10 @@
 # Calma, Eugene Marie S.
 # Calculator
 
+
 # Ask the user to input his name 
 username=input("Hi, Good day! Please enter your name.")
+# Ask the user what kind of mathematical operation it would used.
+print("Welcome to your Virtual Calculator! Enter the numbers and I'll solve it for you!")
+first_number = input("Please enter your first number.").upper().replace(" ", "")
+second_numgber = input("Please enter your second number.").upper().replace(" ", "")
