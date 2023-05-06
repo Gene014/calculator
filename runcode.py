207888172e54f95e9eba466ad23a2ace64c30433
Gene014 <calmaeugene06@gmail.com>
2023-05-06 23:58:01 +0800

    all operations working try again looping added, program running,next is trycatch method and finishing touches.

--- runcode.py
+++ runcode.py
@@ -10,16 +10,22 @@ while True:
     second_number = int(input("Please enter your second number."))
     chosen_operation = input("Press 1 if you want to ADD the variables. Press 2 to SUBTRACT. Press 3 to MULTIPLY. Or press 4 to DIVIDE").upper().replace(" ", "")
     if chosen_operation == "1":
-        print (f"The sum is {first_number + second_number}")
+        print (f"Hey {username}! The sum for your problem is {first_number + second_number}")
         #input code for addition
     elif chosen_operation == "2":
-        print("")
+        print(f"Hey {username}! The difference for your problem is {first_number - second_number}")
         #input code for addition
-    elif chosen_operation == 3:
-        print("")
+    elif chosen_operation == "3":
+        print(f"Hey {username}! The product for your problem is {first_number * second_number}")
         #input code for addition
-    elif chosen_operation == 4:
-        print("")
+    elif chosen_operation == "4":
+        print(f"Hey {username}! The quotient for your problem is {first_number / second_number}")
         #input code for addition
     else:
         print("INVALID SYSNTAX")
+    askyesno = input("\n\33[36mDo you still want to continue? (yes/no):\33[0m ")
+    if askyesno.lower() == 'no':
+            print("\33[41mTerminating Program... ")
+            exit()
+    elif askyesno.lower() == 'yes':
+            continue    
