9d66da3a5dcde0d2713a199b96e4e44f83e43be4
Gene014 <calmaeugene06@gmail.com>
2023-05-08 23:43:24 +0800

    added designs, program running.

--- runcode.py
+++ runcode.py
@@ -3,31 +3,33 @@
 
 while True:
     try:
-        # Ask the user to input his name 
-        username=input(f"Hi, Good day! Please enter your name.")
+            # ask the user to input his name 
+        username=input(f"\n\33[33mHi, Good day! Please enter your name.\n\33[0m")
         if username.isnumeric():
             raise TypeError
-        # Ask the user what kind of mathematical operation it would used. 
-        print("Welcome to your Virtual Calculator! Enter the numbers and I'll solve it for you!")
-        first_number = int(input("Please enter your first number."))
-        second_number = int(input("Please enter your second number."))
-        chosen_operation = input("Press 1 if you want to ADD the variables. Press 2 to SUBTRACT. Press 3 to MULTIPLY. Or press 4 to DIVIDE").upper().replace(" ", "")
-        #input code for addition
+            # ask the user for the given. 
+        print(f"\n\33[35mWelcome to your Virtual Calculator {username} ! Enter the numbers and I'll solve it for you!\33[35m")
+        first_number = int(input("\n\33[96mPlease enter your first number.\33[96m" ))
+        second_number = int(input("\n\33[90mPlease enter your second number. "))
+            # ask the operation to be used
+        chosen_operation = input("\n\33[1mPress \33[32m1\33[0m\33[1m if you want to \33[32mADD\33[0m\33[1m the variables. Press \33[32m2\33[0m\33[1m to \33[32mSUBTRACT\33[0m\33[1m. Press \33[32m3\33[0m\33[1m to \33[32mMULTIPLY\33[0m\33[1m. Or press \33[32m4\33[0m\33[1m to \33[32mDIVIDE\33[0m\33[1m. ")
+            #input code for addition
         if chosen_operation == "1":
-            print (f"Hey {username}! The sum for your problem is {first_number + second_number}")
-        #input code for subtraction
+            print (f"\n\33[43mHey {username}! The sum for your problem is {first_number + second_number}")
+            #input code for subtraction
         elif chosen_operation == "2":
-            print(f"Hey {username}! The difference for your problem is {first_number - second_number}")
-        #input code for multiplication
+            print(f"\n\33[43mHey {username}! The difference for your problem is {first_number - second_number}")
+            #input code for multiplication
         elif chosen_operation == "3":
-            print(f"Hey {username}! The product for your problem is {first_number * second_number}")
-        #input code for division
+            print(f"\n\33[43mHey {username}! The product for your problem is {first_number * second_number}")
+            #input code for division
         elif chosen_operation == "4":
-            print(f"Hey {username}! The quotient for your problem is {first_number / second_number}")
-        # wrong input
+            print(f"\n\33[43mHey {username}! The quotient for your problem is {first_number / second_number}")
+            # wrong input
         else:
             raise ValueError
-        askyesno = input("\n\33[36mDo you still want to continue? (yes/no):\33[0m ")
+            # try again
+        askyesno = input("\n\33[46mDo you still want to continue? (yes/no):\33[0m ")
         if askyesno.lower() == 'no':
             print("\33[41mTerminating Program... ")
             exit()
@@ -38,6 +40,6 @@ while True:
     except TypeError:
         print("Please check your typings to avoid errors! ")
     except ValueError:
-        print("Numbers Only!")
+        print("Numbers Only or Check your typings to avoid errors! ")
     except ZeroDivisionError:
         print("Undefined! Please Try Again!")
